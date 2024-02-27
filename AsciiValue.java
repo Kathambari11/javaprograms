a312676369a6b59b02a0ab5550bd2a15a951bf8a
Kathambari11 <161327465+Kathambari11@users.noreply.github.com>
2024-02-27 20:20:10 +0530

    Add files via upload
    Program to find ASCII values...

---
+++ AsciiValue.java
@@ -0,0 +1,12 @@
+public class AsciiValue   
+{  
+public static void main(String[] args)   
+{   
+char ch1 = 'g';  
+char ch2 = 'k';  
+int ascii1 = (int) ch1;  
+int ascii2 = (int) ch2;  
+System.out.println("The ASCII value of " + ch1 + " is: " + ascii1);  
+System.out.println("The ASCII value of " + ch2 + " is: " + ascii2);  
+}  
+}
